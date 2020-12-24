ede102ec8bbbbb98d1ee8619b55ac3b34814df45
Vladimir Zudenkov <zudenkov2021@yandex.ru>
2020-12-24 11:51:36 +0500

    Второе испарвление ревью

--- javascript/script.js
+++ javascript/script.js
@@ -1,27 +1,21 @@
 let openButton = document.querySelector('.user__edit-profile'),
-  overlay = document.querySelector('.popup'),
-  closeButton = overlay.querySelector('.popup__button-close'),
-  autor = document.querySelector('.user__title'),
-  jobeDescr = document.querySelector('.user__profession'),
-  nameInput = document.querySelector('.popup__text_name'),
-  jobeInput = document.querySelector('.popup__text_description'),
-  formElement = overlay.querySelector('.popup__field');
-
+    overlay = document.querySelector('.popup'),
+    closeButton = overlay.querySelector('.popup__button-close'),
+    autor = document.querySelector('.user__title'),
+    jobeDescr = document.querySelector('.user__profession'),
+    nameInput = document.querySelector('.popup__text_name'),
+    jobeInput = document.querySelector('.popup__text_description'),
+    formElement = overlay.querySelector('.popup__field');
 
 function openForm() {
   overlay.classList.add('popup_active');
   nameInput.value = autor.textContent;
   jobeInput.value = jobeDescr.textContent;
 }
-openButton.addEventListener('click', openForm);
-
-
 
 function closeForm() {
   overlay.classList.remove('popup__active');
 }
-closeButton.addEventListener('click', closeForm);
-
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
@@ -29,4 +23,7 @@ function handleFormSubmit(evt) {
   jobeDescr.textContent = jobeInput.value;
   overlay.classList.remove('popup_active');
 }
+
+openButton.addEventListener('click', openForm);
+closeButton.addEventListener('click', closeForm);
 formElement.addEventListener('submit', handleFormSubmit);
