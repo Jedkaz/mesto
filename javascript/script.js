a3dad928fff657466ee85cf7951dca057b48a2b9
Vladimir Zudenkov <zudenkov2021@yandex.ru>
2021-01-15 15:09:18 +0500

    заготовка для 5 спринта

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,38 @@
-let openButton = document.querySelector('.user__edit-profile'),
-    overlay = document.querySelector('.popup'),
-    closeButton = overlay.querySelector('.popup__button-close'),
-    autor = document.querySelector('.user__title'),
-    jobeDescr = document.querySelector('.user__profession'),
-    nameInput = document.querySelector('.popup__text_name'),
-    jobeInput = document.querySelector('.popup__text_description'),
-    formElement = overlay.querySelector('.popup__field');
+const openButton = document.querySelector('.user__edit-profile'),
+  overlay = document.querySelector('.popup'),
+  closeButton = overlay.querySelector('.popup__button-close'),
+  autor = document.querySelector('.user__title'),
+  jobeDescr = document.querySelector('.user__profession'),
+  nameInput = document.querySelector('.popup__text_name'),
+  jobeInput = document.querySelector('.popup__text_description'),
+  formElement = overlay.querySelector('.popup__field');
+
+
+const initialCards = [{
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 
 function openForm() {
   overlay.classList.add('popup_active');
@@ -24,6 +51,9 @@ function handleFormSubmit(evt) {
   jobeDescr.textContent = jobeInput.value;
   overlay.classList.remove('popup_active');
 }
+
+
+
 
 openButton.addEventListener('click', openForm);
 closeButton.addEventListener('click', closeForm);
