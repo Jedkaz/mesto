27835c5d48c3766fe8f6c86b568ae32dceca7fa2
Vladimir Zudenkov <zudenkov2021@yandex.ru>
2021-02-04 10:32:02 +0500

    6-3

--- javascript/validate.js
+++ javascript/validate.js
@@ -1,64 +1,80 @@
-'use strict';
 
 
+// Функция isValid теперь принимает formElement и inputElement,
+// а не берёт их из внешней области видимости
 
-
-const showInputError = (formElement, inputElement, errorMessage) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.add('form__input_type_error');
-  errorElement.textContent = errorMessage;
-  errorElement.classList.add('form__input-error_active');
-};
-
-const hideInputError = (formElement, inputElement) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.remove('form__input_type_error');
-  errorElement.classList.remove('form__input-error_active');
-  errorElement.textContent = '';
-};
-
-const hasInvalidInput = (inputList) => {
-  return inputList.some((inputElement) => {
-    return !inputElement.validity.valid;
-  });
-};
-
-const toggleButtonState = (inputList, buttonElement) => {
-  console.log(hasInvalidInput(inputList));
-  if (hasInvalidInput(inputList)) {
-    buttonElement.classList.add('button_inactive');
+const isValid = (formSelector, inputSelector) => {
+  if (!inputSelector.validity.valid) {
+    // showInputError теперь получает параметром форму, в которой
+    // находится проверяемое поле, и само это поле
+    showInputError(formSelector, inputSelector, inputSelector.validationMessage);
   } else {
-    buttonElement.classList.remove('button_inactive');
+    // hideInputError теперь получает параметром форму, в которой
+    // находится проверяемое поле, и само это поле
+    hideInputError(formSelector, inputSelector);
   }
+}; 
+
+
+const showInputError = (formSelector, inputSelector, errorMessage) => {
+  // Находим элемент ошибки внутри самой функции
+  const errorElement = formSelector.querySelector(`.${inputSelector.id}-error`);
+  // Остальной код такой же
+  inputSelector.classList.add('popup__input_type_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__input-error_active');
 };
 
-const setEventListeners = (formElement) => {
-  const inputList = Array.from(formElement.querySelectorAll('.form__input'));
-  const buttonElement = formElement.querySelector('.form__submit');
+const hideInputError = (formSelector, inputSelector) => {
+  // Находим элемент ошибки
+  const errorElement = formSelector.querySelector(`.${inputSelector.id}-error`);
+  // Остальной код такой же
+  inputSelector.classList.remove('popup__input_type_error');
+  errorElement.classList.remove('popup__input-error_active');
+  errorElement.textContent = '';
+}; 
 
-  toggleButtonState(inputList, buttonElement);
 
-  inputList.forEach((inputElement) => {
-    inputElement.addEventListener('input', function () {
-      toggleButtonState(inputList, buttonElement);
+
+const setEventListeners = (formSelector) => {
+  // Находим все поля внутри формы,
+  // сделаем из них массив методом Array.from
+  const inputList = Array.from(formSelector.querySelectorAll('.popup__input'));
+
+  // Обойдём все элементы полученной коллекции
+  inputList.forEach((inputSelector) => {
+    // каждому полю добавим обработчик события input
+    inputSelector.addEventListener('input', () => {
+      // Внутри колбэка вызовем isValid,
+      // передав ей форму и проверяемый элемент
+      isValid(formSelector, inputSelector)
     });
   });
-};
+}; 
+
 
 const enableValidation = () => {
+  // Найдём все формы с указанным классом в DOM,
+  // сделаем из них массив методом Array.from
+  //const formList = Array.from(document.formSelector);
   const formList = Array.from(document.querySelectorAll('.popup__form'));
-  formList.forEach((formElement) => {
-    formElement.addEventListener('submit', function (evt) {
+  // Переберём полученную коллекцию
+  formList.forEach((formSelector) => {
+    formSelector.addEventListener('submit', (evt) => {
+      // У каждой формы отменим стандартное поведение
       evt.preventDefault();
     });
 
-    const fieldsetList = Array.from(formElement.querySelectorAll('.form__set'));
-
-    fieldsetList.forEach((fieldset) => {
-      setEventListeners(fieldset);
-    });
+    // Для каждой формы вызовем функцию setEventListeners,
+    // передав ей элемент формы
+    setEventListeners(formSelector);
   });
 };
+
+// Вызовем функцию
+//enableValidation(); 
+
+
 
 enableValidation({
   formSelector: '.popup__form',
@@ -66,10 +82,5 @@ enableValidation({
   submitButtonSelector: '.popup__button',
   inactiveButtonClass: 'popup__button-save_inactive',
   inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__error_visible'
+  //errorClass: 'popup__error_visible'
 });
-
-
-
-
-//enableValidation();
