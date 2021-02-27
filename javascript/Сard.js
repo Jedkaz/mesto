a69dd79acc4e5f4e577b4426ae427f287de49f1f
Vladimir Zudenkov <zudenkov2021@yandex.ru>
2021-02-27 15:40:54 +0500

    исправление ревью 1

---
+++ javascript/Сard.js
@@ -0,0 +1,53 @@
+'use strict';
+
+export default class Card {
+
+  constructor(cards, handlePreview, templateData) {
+    this._name = cards.name;
+    this._link = cards.link;
+    this._handlePreview = handlePreview;
+    this._templateData = templateData;
+
+  }
+
+  _getTemplate() {
+    const cardElement = this._templateData.querySelector('.cards__item')
+      .cloneNode(true);
+    return cardElement;
+  }
+
+  generateCard() {
+
+    this._element = this._getTemplate();
+    this._cardImage = this._element.querySelector('.cards__item-picture');
+    this._cardLike = this._element.querySelector('.cards__item-like');
+    this._cardRemoover = this._element.querySelector('.cards__remoover');
+    this._element.querySelector('.cards__item-heading').textContent = this._name;
+    this._cardImage.src = this._link;
+    this._cardImage.alt = this._name;
+
+    this._setListeners();
+    return this._element;
+
+  }
+
+  _setListeners() {
+    this._cardRemoover.addEventListener('click', () => {
+      this._handleRemoover();
+    });
+    this._cardLike.addEventListener('click', () => {
+      this._handleLike();
+    });
+    this._cardImage.addEventListener('click', () => {
+      this._handlePreview(this._name, this._link);
+    });
+  }
+
+  _handleRemoover() {
+    this._element.remove('cards__item');
+  }
+
+  _handleLike() {
+    this._element.querySelector('.cards__item-like').classList.toggle('cards__item-like_active');
+  }
+}
